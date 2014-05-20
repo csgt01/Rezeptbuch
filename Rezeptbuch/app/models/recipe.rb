0fa9b30835f3f33e7730250715b84fa9bade85eb
Christian Schulte <c.schulte@geomobile.de>
2014-05-20 15:30:57 +0200

    tsting

--- Rezeptbuch/app/models/recipe.rb
+++ Rezeptbuch/app/models/recipe.rb
@@ -1,10 +1,9 @@
 class Recipe < ActiveRecord::Base
 	has_many :quantities, inverse_of: :recipe
-	has_many :ingredients, 
+	has_many :ingredients,
            :through => :quantities, inverse_of: :recipe
 
-    accepts_nested_attributes_for :quantities, 
-           :reject_if => :all_blank, 
+  accepts_nested_attributes_for :quantities,
+           :reject_if => :all_blank,
            :allow_destroy => true
-    accepts_nested_attributes_for :ingredients
 end
